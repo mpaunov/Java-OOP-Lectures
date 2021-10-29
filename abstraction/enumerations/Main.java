d9d57cc5df237def82850e67524f8a5b7cd773f5
Martin Paunov <paunovmar@gmail.com>
2021-10-29 18:44:57 +0300

    Main will now hold the logic for reading and printing the vacation price.

--- abstraction/enumerations/Main.java
+++ abstraction/enumerations/Main.java
@@ -1,11 +1,21 @@
 package abstraction.enumerations;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        DayOfWeek[] values = DayOfWeek.values();
+        Scanner scanner = new Scanner(System.in);
 
-        for (DayOfWeek value : values) {
-            System.out.println(value.getName());
-        }
+        String[] input = scanner.nextLine().split("\\s+");
+
+        double pricePerDay = Double.parseDouble(input[0]);
+        int numberOfDays = Integer.parseInt(input[1]);
+        String season = input[2];
+        String discountType = input[3];
+
+        PriceCalculator calculator = new PriceCalculator(
+                pricePerDay, numberOfDays, Season.fromString(season), Discount.fromString(discountType));
+
+        System.out.printf("%.2f%n", calculator.calculate());
     }
 }
